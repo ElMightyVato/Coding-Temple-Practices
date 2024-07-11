15f1b5529ae33f778ae48778c00878b864d359b5
ElMightyVato <jairgranadosramirez@gmail.com>
2024-07-11 00:49:12 -0500

    Exercises

---
+++ Testing.py
@@ -0,0 +1,8 @@
+light_color = input("Enter the traffic light color (red, yellow,green):")
+
+if light_color == 'red':
+    print("Stop!")
+elif light_color == "yellow":
+    print("Slow down and prepare to stop.")
+else:
+    print("Go!")
