033ea28e2420ec0842dc7356fc225c24bf5a8e57
ElMightyVato <jairgranadosramirez@gmail.com>
2025-02-28 08:59:58 -0600

    More updates!

---
+++ M10-JavaScript/script.js
@@ -0,0 +1,75 @@
+// Output methods
+alert('Hello, world2!') //Using alert the pop up box that appears
+console.log('Hello, world!'); // Logging to the console => print()
+document.write('Hello, world!'); // Writing directly to the document > this puts it onto our html page ourselves
+
+// This is a single-line comment #
+
+/*
+    This.is.a
+    multi-line comment
+*/
+
+//using main.py to compare 
+
+// var, let, const
+
+// var name1 = 'John Smith' // Var is outdated you DO NOT need to use var
+
+// name1 = 'Jane Smith' // Reassigning the value
+
+// var name1 = 'Joe Smith' //Declaring the value
+
+// let name1 = 'John Smith' // Will not let you redeclare the value.
+
+// name1 = 'Joe Smith' // Will let you reassign.
+
+// let name1 = 'Joe Smith' // Example of not letting you redclare
+
+// const name1 = 'John Smith' // If you have const you cannot change it
+
+// name1 = 'Jane Smith'
+
+/*
+Strings
+Numbers
+Boolenas
+Array => List
+Object ==> Dictionary
+*/
+
+// Declare variables
+// let name = "John";
+// let age = 30;
+// let isStudent = true;
+// let fruits = ["apple", "banana", "Orange"];
+// let person = { name: "Alice", age: 25};
+
+// // Output variables to the console
+// console.log("Name:" name);
+// console.log("Age:", age);
+// console.log("Is student:", isStudent);
+// console.log("Fruits", fruits);
+// console.log("Person", person);
+
+// // Perform logical operation
+// let isvalid = true \\ false
+// console.log("Is it valid?", isvalid);
+
+// Type Conversion
+// console.log(typeof 42); // output: "umber"
+// console.log(typeof "Hello,world!"); // output: "string"
+// console.log(typeof true); // Output: "Boolean"
+// console.log(typeof {name: "john", age: 30 }); //output: "Object"
+// console.log(typeof [1,2,3]); // Output: "Object"
+// console.log(typeof undefined); // Output: "undefined"
+
+// // Type Coercion
+
+// console.log(5 + '5') //=> '55' Converts it to a string
+// console.log("10" * 5) //=> 5
+// console.log("true" == true) // => false
+// console.log(+"100") // ==> 100
+
+// console.log("4" == 4) // ==> If the value is equal
+// console.log("4" === 4) // ==> If the data type and value are equal
